a519937ae30d82102333ae3cf5a26164fb195bfd
bharadwaj21122001 <bharadwaj.21122001@gmail.com>
2024-08-13 09:55:41 +0530

    peak element using binary search

--- peak_element_bs.py
+++ peak_element_bs.py
@@ -1,13 +1,13 @@
 def peakElement(arr, n):  
-    i=0
-    j=n-1
+    i = 0
+    j = n-1
     
     while i<j:
         mid = (i+j)//2
         
         if arr[mid] >= arr[mid+1]:
-            j=mid
+            j = mid
         else:
-            i=mid+1
+            i = mid+1
     return i
 print(peakElement([1,2,3,4], 4))
