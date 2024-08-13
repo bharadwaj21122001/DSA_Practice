e80cc3b2519e8995b78d722bc330b3a9398e0a35
bharadwaj21122001 <bharadwaj.21122001@gmail.com>
2024-08-13 10:16:45 +0530

    DSA min and max

---
+++ min_max.py
@@ -0,0 +1,12 @@
+def get_min_max(arr):
+    min_ele = arr[0]
+    max_ele = arr[len(arr)-1]
+    for i in range(len(arr)):
+        if arr[i] < min_ele:
+            min_ele = arr[i]
+        if arr[i] > max_ele:
+            max_ele = arr[i]
+    return min_ele, max_ele
+
+print(get_min_max([3,2,1,5,1000,6,7]))
+print(get_min_max([34353,64545,234,67544]))
