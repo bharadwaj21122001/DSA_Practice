f990a37ce6822317b0b05c3e616d6c766afa9f0a
bharadwaj21122001 <bharadwaj.21122001@gmail.com>
2024-09-10 23:53:31 +0530

    Check the array is sorted.

--- check_sorted.py
+++ check_sorted.py
@@ -5,12 +5,14 @@
 def check(nums):
     n = len(nums)
     count = 0
+    
     for i in range(n):
         if nums[i] > nums[(i+1)%n]:
             count += 1
         if count > 1:
             return False
     return True
+
 print(check([3,4,5,1,2]))
 print(check([2,1,3,4]))
 print(check([1,1,1]))   
