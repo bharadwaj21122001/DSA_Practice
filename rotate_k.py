440220c81152690a73f40f87a6ff632be5b4ca94
bharadwaj21122001 <bharadwaj.21122001@gmail.com>
2024-09-13 16:36:35 +0530

    Rotating the array by given number of times.

--- rotate_k.py
+++ rotate_k.py
@@ -2,9 +2,19 @@ def rotate(nums, k):
         """
         Do not return anything, modify nums in-place instead.
         """
-        b = (len(nums) - k) % len(nums)
-        nums[:] = nums[b:] + nums[:b]
+        # b = (len(nums) - k) % len(nums)
+        # nums[:] = nums[b:] + nums[:b]
+        # return nums
+
+        n = len(nums)
+        b = k % n
+
+        temp = nums[-b:] + nums[:-b]
+
+        for i in range(n):
+            nums[i] = temp[i]
         return nums
+
 
 print(rotate([1,2,3,4,5,6,7], 3))
 print(rotate([-1,-100,3,99], 2))
