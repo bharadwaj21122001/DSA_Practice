23deea2a7ff2ed1d7f96ecbbbe8cdf0eea6834c9
bharadwaj21122001 <bharadwaj.21122001@gmail.com>
2025-03-10 17:49:20 +0530

    Reversing a singly linked list in iterative method.

---
+++ Reversing_LL.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def reverse(self, head):
+        prev = None
+        curr = head
+
+        while curr:
+            next_node = curr.next
+            curr.next = prev
+            prev = curr
+            curr = next_node
+        return prev
